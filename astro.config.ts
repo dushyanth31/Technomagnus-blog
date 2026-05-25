cec05d8f590c2cf1bdfffff8caeb4683eeaad2ab
DanUsh41 <86404304+dushyanth31@users.noreply.github.com>
2026-05-25 19:36:44 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -72,5 +72,11 @@ export default defineConfig({
   },
   experimental: {
     svgOptimizer: svgoOptimizer(),
+    astro.config.mjs
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
   },
 });
